068b1fd590270a346aa13be5cdd0cb98cb935b68
Марат Шайхнисламов <sharumario@mail.ru>
2022-07-31 17:22:57 +0500

    Скорректировал CommentSerializer, Добавил read_only в Meta класс.

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -24,14 +24,13 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
-        read_only=True,
+        read_only=True
     )
-    post = serializers.PrimaryKeyRelatedField(
-        read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
 
 
 class FollowSerializer(serializers.ModelSerializer):
